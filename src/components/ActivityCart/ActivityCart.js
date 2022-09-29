b4397db8d028ab6fd48e014bfd7b1f8e9026f657
raihan-778 <raihanarif778@gmail.com>
2022-09-29 16:52:45 +0600

    sweet aleart added

--- src/components/ActivityCart/ActivityCart.js
+++ src/components/ActivityCart/ActivityCart.js
@@ -1,12 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ActivityCart.css";
+import { toast } from "react-toastify";
+
+// Import toastify css file
+import "react-toastify/dist/ReactToastify.css";
+
+import Swal from "sweetalert2";
+
+// CommonJS
 
 const ActivityCart = ({ duration }) => {
   const [breakTime, setBreakTime] = useState("");
+
+  useEffect(() => {
+    const storedData = findBreakTime();
+    console.log(storedData);
+  }, [breakTime]);
   // Break time event handler
+
   const handleShowBreakTime = (e) => {
     console.log(e.target.innerHTML);
+    localStorage.setItem("breakTime", e.target.innerHTML);
     setBreakTime(e.target.innerHTML);
+  };
+
+  const findBreakTime = () => {
+    let getStoredBreakTime = {};
+
+    //get the shopping cart from local storage
+    const storedTime = localStorage.getItem("breakTimer");
+    if (storedTime) {
+      getStoredBreakTime = JSON.parse(storedTime);
+    }
+    return getStoredBreakTime;
+  };
+
+  //Toast animations
+  const handelToast = () => {
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Welldone! You have completed the task",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   };
 
   return (
@@ -32,6 +69,7 @@ const ActivityCart = ({ duration }) => {
           </button>
         </div>
         <div className="study-duration">
+          <h2>My Learning Details</h2>
           <h2>
             Learning Time
             <span className="duration-count">{duration} days</span>
@@ -40,7 +78,9 @@ const ActivityCart = ({ duration }) => {
             Break Time <span className="duration-count">{breakTime}</span>
           </h2>
         </div>
-        <button className="complited-btn">Activity Complited</button>
+        <button onClick={handelToast} className="complited-btn">
+          Activity Complited
+        </button>
       </div>
     </div>
   );
